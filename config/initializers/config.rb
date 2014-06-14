b4fd9dabf6f31d68e390103f63e0e08bc85db3e2
Julian Nadeau <julian.nadeau@jadedpixel.com>
2014-06-14 17:44:08 -0400

    Fix config settings

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,7 +1,1 @@
-if Rails.env.production?
-  CONFIG = YAML.load_file(File.expand_path("/home/deploy/cfktracker/shared/config/config.yml"))[Rails.env]
-elsif Rails.env.test?
-  CONFIG = YAML.load_file(File.expand_path("config/sample.config.yml"))[Rails.env]
-else
-  CONFIG = YAML.load_file(File.expand_path("config/config.yml"))[Rails.env]
-end
+CONFIG = YAML.load_file(File.expand_path("config/config.yml"))[Rails.env]
